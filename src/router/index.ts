1a7dac7deb8ac26d8775614db4c4365801ba528a
Marry <2112791909@qq.com>
2021-11-11 22:11:26 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,15 @@ import VueRouter  from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics  from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
 {
-path:'/',
-redirect:''
+//路由默认的路径首页，匹配的逻辑是从上面到下面来匹配
+ path:'/',
+ redirect:'/money'
 },
   {
    path:'/money',
@@ -23,7 +25,10 @@ redirect:''
    path:'/statistics',
    component:Statistics
  },
-
+{
+  path:'*',
+  component: NotFound,
+}
 ]
 
 const router = new VueRouter({
