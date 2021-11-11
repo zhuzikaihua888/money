7049d6313396f8cfefd1329b20f63c1ea89dffc6
Marry <2112791909@qq.com>
2021-11-11 23:17:47 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-    <div> Money.vue
+    <div class="nav-wrapper"> 
+   <div class="content">
+   Money.vue
+   </div>
     <Nav/>
     </div>
 </template>
@@ -11,7 +14,15 @@
         
     }
 </script>
-
 <style lang="scss" scoped>
-
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+}
+.content{
+    overflow:auto;
+    flex-grow: 1;
+}
 </style>
