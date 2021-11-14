ec2122fff017556aafe671124bb925cb222ef8c1
Marry <2112791909@qq.com>
2021-11-14 21:58:57 +0800

    创建Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+    <svg class="icon">
+   <use :xlink:href="`#`+name"/>
+    </svg>
+</template>
+
+<script lang="ts">
+   let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext); //这里ts语言不认识requireContext，需要明确其类型
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (err) {
+  console.log(err);
+}
+   export default {
+     props:['name'],
+     name:'Icon'
+     
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .icon {
+       width: 1em; height: 1em;
+       vertical-align: -0.15em;
+       fill: currentColor;
+       overflow: hidden;
+    }
+
+</style>
