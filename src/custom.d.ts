b39dead4048eaa8bd3fe3e5ec418a945cace85ab
Marry <2112791909@qq.com>
2021-12-07 22:25:20 +0800

    封装model.ts

---
+++ src/custom.d.ts
@@ -0,0 +1,7 @@
+type RecordItem={
+    tages:string[]
+    notes:string
+    type:string
+    amount:number//数据类型 对象下面有类
+    createdAt:Date//类
+  } 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -4,7 +4,7 @@ import content from '../shims-vue';
 <template>
 <div>
  <Layout class-prefix="layout">  
-   {{recordlist}}
+   {{recordList}}
    <NumberPad @update:value="onupdataAmount"  @x="Recorddata"/>
    <Types :value.sync="record.type" />
    <Notes @update:value="onupdataNotes"/>
@@ -20,40 +20,41 @@ import NumberPad from '@/components/NumberPad.vue';
 import Types from '@/components/Types.vue';
 import Notes from '@/components/Notes.vue';
 import Tags from '@/components/Tags.vue';
+import model from '@/model';
+
+
+const recordList:RecordItem[]=model.fetch()
+
+
 //申明类型
-type Record={
-  tages:string[]
-  notes:string
-  type:string
-  amount:number//数据类型 对象下面有类
-  createdAt:Date//类
-} 
+
  
  @Component({
         components: { NumberPad, Types, Notes, Tags } //组件引用
     })
   export default class Money  extends Vue{
     tags=['衣','食','住','行',];
-   //JSON.parse解析字符串
-   recordlist:Record[]=JSON.parse(window.localStorage.getItem('recordlist') || '[]')
-   record:Record={tages:[],notes:'',type:'-',amount:0,};
+   //JSON.parse解析字符串获取数据
+  recordList: RecordItem[] = recordList;
+   record:RecordItem={tages:[],notes:'',type:'-',amount:0,};
    onupdataTags(value:string []){
    this.record.tages=value;
    };
-   onupdataNotes(value:string){
+   onupdataNotes(value:string){ 
    this.record.notes=value;
    };
   onupdataAmount(value:string){
      this.record.amount=parseFloat(value);
    };
    Recorddata(){
-     const record2:Record=JSON.parse(JSON.stringify(this.record))
+     const record2:RecordItem=JSON.parse(JSON.stringify(this.record))
      record2.createdAt=new Date()
-     this.recordlist.push(record2)
+     this.recordList.push(record2)
    }
-@Watch('recordlist')
+@Watch('recordList')
 onRecorddataChange(){
-  window.localStorage.setItem('recordlist',JSON.stringify(this.recordlist))
+ //保存数据
+ model.save(this.recordList)
 }
     }
 </script>
@@ -61,8 +62,7 @@ onRecorddataChange(){
 <style lang="scss">
 .layout-content{ 
     display: flex;
-    flex-direction: column-reverse;
-   
+    flex-direction: column-reverse;   
 };
 </style>
 
