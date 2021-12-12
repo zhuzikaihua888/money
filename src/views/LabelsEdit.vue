d04c16a9dad444cb624200a349ea62b9a1ef0746
Marry <2112791909@qq.com>
2021-12-12 20:53:42 +0800

    note组件css不生效

--- src/views/LabelsEdit.vue
+++ src/views/LabelsEdit.vue
@@ -2,12 +2,19 @@ import Layout from '@/components/Layout.vue';
 <template>
   
 <Layout>
-<div>
- <Icon name="left"/>
-  <span>编辑标签</span>
+<div class="navBar">
+ <Icon name="left" class="leftIcon"/>
+  <span class="title">编辑标签</span>
+  <span class="rightIcon"></span>
 </div>
+
+<div class="from-wrapper"  >
 <Notes fieldName="标签" placeholder="请输入标签名" />
-<Button>删除标签</Button>
+</div>
+
+<div class="button-wrapper">
+<Button >删除标签</Button>
+</div>
 
 </Layout>
 </template>
@@ -33,13 +40,37 @@ import Layout from '@/components/Layout.vue';
         }else{
        //可以回退到之前的页面
           this.$router.replace('/404');
-        }
-        
-      }
-       
+        }    
+      }     
     };
 </script>
 <style lang="scss" scoped>
-
-
+ .navBar {
+    text-align: center;
+    font-size: 16px;
+    padding: 12px 16px;
+    background: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    > .title {
+    }
+    > .leftIcon {
+      width: 24px;
+      height: 24px;
+    }
+    > .rightIcon {
+      width: 24px;
+      height: 24px;
+    }
+  }
+  .form-wrapper{
+    background:white;
+    margin-top: 8px;
+  }
+  .button-wrapper{
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
 </style>
